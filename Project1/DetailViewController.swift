7bda42f93e8a6d23ecd08949d192ff9025597cfc
Anthony Beckford <abeckford532@gmail.com>
2022-10-13 08:40:32 -0400

    added a detail screen that loads the pictures, made the screen animated when the user selects an image name. Learned about  UIImageView, UIImage,  and UINavigationBar

---
+++ Project1/DetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Anthony Beckford on 10/13/22.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       // checking to see if selectedImage has a value
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
